7cb0c3d5f3ac7fe73a9c2a94426e10e3e11a2d2e
MuhammadSami223 <muhammadsamiahsan@gmail.com>
2024-01-06 17:04:16 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import About from './components/about/About'
 // import Value from './components/value/VAlue'
-import Values from './components/value/VAlue'
+import Values from './components/value/Value'
 import Downloads from './components/DownloadApp/Downloads'
 import Footer from './Footer/Footer'
 
